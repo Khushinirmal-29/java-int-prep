37a44b8fa04062d2982277a16f5b54f3c227028d
Khushi Nirmal <khushinirmal7136@gmail.com>
2026-01-06 10:34:52 +0530

    Single Inheritance

--- Main_Inheritance.java
+++ Main_Inheritance.java
@@ -1,3 +1,32 @@
+class ClassyCar{
+    String color;
+    int speed;
+
+
+    void start(){
+        System.out.println("ClassyCar is starting");
+    }
+    void drive(){
+        System.out.println("ClassyCar is driving at speed : " + speed);
+    }
+}
+
+class SportsCar extends ClassyCar{
+    void turboon(){
+        System.out.println("Turbo Mode ON! ");
+    }
+}
+
+
 public class Main_Inheritance {
+    public static void main(String[] args){
+        SportsCar sc=new SportsCar();
+        sc.color="White";
+        sc.speed=200;
+
+        sc.start();
+        sc.drive();
+        sc.turboon();
+    }
     
 }
